fbe939effee8a96a19308feef9801867342ca8b6
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2021-02-25 18:06:59 +0900

    210225

--- project_210221/js/main.js
+++ project_210221/js/main.js
@@ -1,6 +1,17 @@
+$(window).scroll(function(){
+  /* 페럴렉스 코드 */
+});
+
+$(".nav_main>li>a").click(function(){
+    const menu_title = $(this).attr('name');
+    const y = $('.' + menu_title).offset().top;
+    window.scrollTo({top : y, behavior: 'smooth'});	
+    
+});
+
 $(".nav_slide").mouseenter(function(){
-    $(this).children(".nav_sub").stop().addClass('on');
+    $(this).children(".nav_sub").stop().slideDown(500);
 })
 $(".nav_slide").mouseleave(function(){
-    $(this).children(".nav_sub").stop().removeClass('on');
+    $(this).children(".nav_sub").stop().slideUp(500);
 })
