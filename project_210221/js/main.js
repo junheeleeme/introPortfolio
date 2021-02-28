3444a410490d5bb3cb73e02072c535ed2be4d366
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2021-02-28 23:33:52 +0900

    210228

--- project_210221/js/main.js
+++ project_210221/js/main.js
@@ -1,16 +1,98 @@
-$(window).scroll(function(){
-  /* 페럴렉스 코드 */
+let scrollTop;
+let isWheel_move;
+
+function checkMobile(){ //접속 환경 구분
+    var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
+ 
+    if ( varUA.indexOf('android') > -1) {
+        //안드로이드
+        return 'android';
+    } else if ( varUA.indexOf("iphone") > -1||varUA.indexOf("ipad") > -1||varUA.indexOf("ipod") > -1 ) {
+        //IOS
+        return 'ios';
+    } else {
+        return 'other';
+    } 
+}
+
+window.addEventListener('mousewheel', (e) => {
+    /*
+    scrollTop = window.innerHeight;
+    if(e.wheelDelta === -120){
+        console.log(e);
+    } else{
+        console.log(e.wheelDelta);
+    }*/
+    console.log(e.wheelDelta);
+    clearInterval(isWheel_move);
+    isWheel_move = setTimeout(() => isWheel(e.wheelDelta), 200);
+
 });
 
-$(".nav_main>li>a").click(function(){
-    const menu_title = $(this).attr('id');
-    const y = $('.' + menu_title).offset().top;
-    window.scrollTo({top : y, behavior: 'smooth'});	
-});
+function isWheel(wDelta){
+    if(wDelta > 0 ){
+        move_up();
+    }
+    else{
+        move_down();
+    }
+}
 
-$(".nav_slide").mouseenter(function(){
-    $(this).children(".nav_sub").stop().slideDown(500);
+
+/* 메뉴 이동  */
+let select_nav = 0;
+// 
+//0 : Profile
+//1 : My Skills
+//2 : Portflio
+//3 : Contact me
+
+    /* nav메뉴 직접 클릭 이동 */
+const target = document.querySelectorAll(".nav_main > li > a");
+
+for(i = 0 ; i < target.length ; i++){ //네비게이션 메뉴 전체 이벤트 등록
+    target[i].addEventListener('click', () =>{
+        move_nav(event.target.getAttribute("id")); //클릭한 타겟 id값 확인
+
+        for(i=0 ; i< target.length ; i++){ //직접 클릭한 메뉴 위치 저장
+            if(target[i].getAttribute('id') === event.target.getAttribute("id")){
+                select_nav = i;
+            }
+        }
+    })    
+}  
+
+function move_nav(target_id){
+    const target_nav = document.querySelector('.'+target_id);
+    const targetTop = window.pageYOffset + target_nav.getBoundingClientRect().top;
+    
+    if (checkMobile() === 'ios'){ //접속 환경 구분
+        $('html').animate({scrollTop: targetTop.toFixed(4)}, 200);
+    }else{
+        window.scroll({top : targetTop.toFixed(4), behavior: 'smooth'});
+    }
+    console.log((targetTop).toFixed(4));
+}    /* nav메뉴 직접 클릭 이동 */
+
+    /* 업다운 버튼 클릭 이동 */
+document.querySelector('#up').addEventListener('click',() =>{
+    move_up();
 })
-$(".nav_slide").mouseleave(function(){
-    $(this).children(".nav_sub").stop().slideUp(500);
+
+document.querySelector('#down').addEventListener('click',() =>{   
+    move_down();
 })
+
+function move_up(){
+    if(select_nav > 0) select_nav -= 1;
+    move_nav(target[select_nav].getAttribute('id'));
+}
+
+function move_down(){
+    if(select_nav < target.length-1) select_nav += 1;
+    move_nav(target[select_nav].getAttribute('id'));
+}
+    /* 업다운 버튼 클릭 이동 */
+/* 메뉴 이동  */
+
+
