0569aa23654fa1ec4c54cae4224d21161ef766e5
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2021-03-12 20:45:37 +0900

    210312_KeyFunc(done)

--- project_210221/js/main.js
+++ project_210221/js/main.js
@@ -2,16 +2,30 @@
 let scrollTop;
 let isWheel_move;
 let wDelta; //마우스 Delta값
-let nav_idx = 0; //nav 메뉴위치
-
-function init(){
-    nav_focus(nav_idx);
-}
+let nav_idx = 0; 
 
 init();
 
+function init(){
+    nav_focus(nav_idx);
+    fnMove(nav_idx);
+}
 
-/* 마우스 스크롤 기능 구현 */
+/* 키보드 화살표 기능구현 */
+$(this).on('keydown', function(e){
+    
+    if(e.keyCode === 38){ // ArrowUp
+        console.log(e.code);
+        fn_moveUp();
+    }
+    else if(e.keyCode === 40){ // ArrowDown
+        console.log(e.code);
+        fn_moveDown();
+    }
+})
+/* 키보드 화살표 기능구현 */
+
+/* 마우스 스크롤 기능구현 */
 $(this).on("mousewheel DOMMouseScroll", function (e) { 
 
     clearInterval(isWheel_move);
@@ -25,26 +39,28 @@ function isWheel(wDelta){ // 마우스 휠 방향 구분
     if(wDelta > 10){
         fn_moveUp();
     }
-    else if(wDelta === 3){
+    else if(wDelta === 3 || wDelta === 0){
         fn_moveUp();
     }
     else if(wDelta < 0){
         fn_moveDown();
     }
 }
-/* 마우스 스크롤 기능 구현 */
+/* 마우스 스크롤 기능구현 */
 
-/* 메뉴 이동 기능 구현 */
+/* 화면 스크롤 이동 기능구현 */
 
-// 
-//0 : Profile
-//1 : My Skills
-//2 : Portflio
-//3 : Contact me
+// nav_idx =>
+//0 : Intro
+//1 : Profile
+//2 : My Skills
+//3 : Portflio
+//4 : Contact me
 
 $(".nav_main>li").click(function(){
-    fnMove($(this).prop('id'));
     nav_idx = $(this).index();
+
+    fnMove(nav_idx);
     nav_focus(nav_idx);
 });
 
@@ -59,7 +75,7 @@ $("#down").click(function(){
 function fn_moveUp(){   //화면 위로 이동
     if( nav_idx > 0){
         nav_idx-=1;
-        fnMove($('.nav_main>li').eq(nav_idx).prop('id'));
+        fnMove(nav_idx);
         nav_focus(nav_idx);
     }
 }
@@ -67,16 +83,18 @@ function fn_moveUp(){   //화면 위로 이동
 function fn_moveDown(){  //화면 아래로 이동
     if( nav_idx < $(".nav_main>li").length-1){
         nav_idx+=1;
-        fnMove($('.nav_main>li').eq(nav_idx).prop('id'));
+        fnMove(nav_idx);
         nav_focus(nav_idx);
     }
 }
 
-function fnMove(target_id){  //직접 클릭한 메뉴화면 절대위치를 계산해 이동
-    targetTop = $('.'+target_id).offset().top;
+function fnMove(n_idx){  //직접 클릭한 메뉴화면 절대위치를 계산해 이동
+
+    targetTop = $(".main>section").eq(n_idx).offset().top;
     $("html,body").stop().animate({
         scrollTop: targetTop.toFixed(4)}, 500, 'swing');
         console.log((targetTop).toFixed(4));
+
 }
 
 function nav_focus(n_idx){
@@ -86,5 +104,4 @@ function nav_focus(n_idx){
     select.siblings().children('span').removeAttr('style');
     select.siblings().children('a').removeAttr('style');
 }
-
-/* 메뉴 이동 기능 구현 */
+/* 화면 스크롤 이동 기능구현 */
