743ea218fc99d1d34fe4a55a4ba23117bc5db600
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2021-04-05 01:13:40 +0900

    제이쿼리 제거 및 html 구조변경

--- project_210221/js/main.js
+++ project_210221/js/main.js
@@ -1,5 +1,9 @@
+const html = document.querySelector('html');
+const nav_menu = document.querySelectorAll('.nav_main>li');
+const up = document.querySelector('.up');
+const down = document.querySelector('.down');
 
-let scrollTop;
+let chk_dev;
 let isWheel_move;
 let wDelta; //마우스 Delta값
 let nav_idx = 0; 
@@ -11,8 +15,17 @@ function init(){
     fnMove(nav_idx);
 }
 
+/* 스크롤 효과를 위한 iOS 구분 */
+function check_device(){
+    
+    const usr_agent = navigator.userAgent.match(/iPhone|iPad|/) ? false : true;
+    return usr_agent; //ios = true
+
+}
+/* iOS 기기 구분 */
+
 /* 키보드 화살표 기능구현 */
-$(this).on('keydown', function(e){
+html.addEventListener('keydown', (e)=>{
     
     if(e.keyCode === 38){ // ArrowUp
         console.log(e.code);
@@ -26,12 +39,13 @@ $(this).on('keydown', function(e){
 /* 키보드 화살표 기능구현 */
 
 /* 마우스 스크롤 기능구현 */
-$(this).on("mousewheel DOMMouseScroll", function (e) { 
-
-    clearInterval(isWheel_move);
-    wDelta = e.originalEvent.wheelDelta;
-    isWheel_move = setTimeout(() => isWheel(wDelta), 200);
-    console.log(e.originalEvent.wheelDelta);
+html.addEventListener('mousewheel', (e)=>{ 
+    
+    if(!e.ctrlKey){
+        clearInterval(isWheel_move);
+        wDelta = e.wheelDelta;
+        isWheel_move = setTimeout(() => isWheel(wDelta), 200);
+    }
 
 });
 
@@ -57,23 +71,39 @@ function isWheel(wDelta){ // 마우스 휠 방향 구분
 //3 : Portflio
 //4 : Contact me
 
-$(".nav_main>li").click(function(){
-    nav_idx = $(this).index();
+nav_menu.forEach(nav_click =>{
+    nav_click.addEventListener('click', ()=>{
+        
+        const click_classN = nav_click.className; 
 
+        for(let i=0 ; i < nav_menu.length ; i++){
+            const parent_child = nav_click.parentElement.children[i];
+            if(parent_child.className === click_classN ){
+                nav_idx = i;
+                fnMove(nav_idx);
+                nav_focus(nav_idx);
+            }
+        }
+    })
+})
+    /*
+    nav_idx = 
     fnMove(nav_idx);
-    nav_focus(nav_idx);
-});
+    nav_focus(nav_idx);*/
 
-$("#up").click(function(){
+up.addEventListener('click', ()=>{
     fn_moveUp();
 })
-
-$("#down").click(function(){
+    
+down.addEventListener('click', ()=>{
     fn_moveDown();
 })
 
+
 function fn_moveUp(){   //화면 위로 이동
-    if( nav_idx > 0){
+    const avail_nav = nav_idx > 0 ? true : false;
+    
+    if(avail_nav){
         nav_idx-=1;
         fnMove(nav_idx);
         nav_focus(nav_idx);
@@ -81,7 +111,9 @@ function fn_moveUp(){   //화면 위로 이동
 }
 
 function fn_moveDown(){  //화면 아래로 이동
-    if( nav_idx < $(".nav_main>li").length-1){
+    const avail_nav = nav_idx < nav_menu.length-1 ? true : false;
+
+    if(avail_nav){
         nav_idx+=1;
         fnMove(nav_idx);
         nav_focus(nav_idx);
@@ -89,19 +121,26 @@ function fn_moveDown(){  //화면 아래로 이동
 }
 
 function fnMove(n_idx){  //직접 클릭한 메뉴화면 절대위치를 계산해 이동
+    const m_section = document.querySelectorAll('.main>section');
+    const targetTop = m_section[n_idx].offsetTop;
 
-    targetTop = $(".main>section").eq(n_idx).offset().top;
-    $("html,body").stop().animate({
-        scrollTop: targetTop.toFixed(4)}, 500, 'swing');
-        console.log((targetTop).toFixed(4));
-
+    if(!check_device()){ // pc/android
+        window.scroll({ top:  targetTop, left: 0, behavior: 'smooth'});
+    }else{ //ios
+        $('html').stop().animate({ scrollTop : targetTop }, 350, 'swing');
+    }
 }
 
 function nav_focus(n_idx){
-    let select = $(".nav_main>li").eq(n_idx);
-    select.children('a').css({'color' : '#fff'});
-    select.children('span').css({'right' : '-63px'});
-    select.siblings().children('span').removeAttr('style');
-    select.siblings().children('a').removeAttr('style');
+
+    nav_menu.forEach(nav=>{
+        nav.children[0].style.color = "#000";
+        nav.children[1].style.right = "500px";
+        nav.children[2].style.right = "500px";
+    })
+
+    nav_menu[n_idx].children[0].style.color = "#fff";
+    nav_menu[n_idx].children[1].style.right = "-63px";
+    nav_menu[n_idx].children[2].style.right = "-63px";
 }
 /* 화면 스크롤 이동 기능구현 */
