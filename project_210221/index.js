a82daa92546db75f69d2ddc6bc836276763529d8
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2021-05-12 03:44:11 +0900

    index.js 작성 및 todo_play 추가

--- project_210221/index.js
+++ project_210221/index.js
@@ -1,17 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const port = 8080;
 
-const mongoose = require('mongoose');
-require('dotenv').config({path: 'mongo_url.env'});
-const MONGODB_URL = process.env.MONGODB_URL;
-
-
-mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, err=>{
-  if(err){
-    console.log('err')
-  }
-  else{
-    console.log('success!')
-
-  }
+const web = http.createServer(function(req, res){
+    let url = req.url;
+    console.log(url);
+    if(url === '/'){
+        url = '/index.html';
+    }
+    if(url === '/favicon.ico'){
+        return res.writeHead(404);
+    }
+    res.writeHead(200);
+    res.end(fs.readFileSync(__dirname + url));
 });
+
+web.listen(port);
+
 
 
